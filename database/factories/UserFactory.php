fd7c5ba0316ef9093c90033a7a088254a58fefcc
d4ism3l <d4ism3l@gmail.com>
2024-06-18 00:33:34 -0400

    Update UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,7 +25,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+			'username' => fake()->name(),
+            'first_name' => fake()->name(),
+			'last_name' => fake()->last_name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
