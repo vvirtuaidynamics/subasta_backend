7a2d7cfc432710ead3b89de24f10c7b7a49a98b2
d4ism3l <d4ism3l@gmail.com>
2024-06-28 02:03:56 -0400

    Trabajo en el modulo de auth.

--- app/Http/Api/Auth/AuthController.php
+++ app/Http/Api/Auth/AuthController.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-
     protected $authService;
 
     public function __construct()
@@ -20,82 +19,22 @@ class AuthController extends Controller
         $this->authService = new AuthService();
     }
 
-    /**
-     * @OA\Post(
-     *     path="/api/register",
-     *     tags={"Auth"},
-     *     summary="Register a new user",
-     *     @OA\RequestBody(
-     *         required=true,
-     *         @OA\JsonContent(
-     *             required={"username","name","email","password"},
-     *             @OA\Property(property="username", type="string", example="john"),
-     *             @OA\Property(property="name", type="string", example="John Doe"),
-     *             @OA\Property(property="email", type="string", format="email", example="john@example.com"),
-     *             @OA\Property(property="password", type="string", format="password", example="password")
-     *         )
-     *     ),
-     *     @OA\Response(response=201, description="User registered successfully"),
-     *     @OA\Response(response=400, description="Bad request")
-     * )
-     */
-    public function register(Request $request): \Illuminate\Http\JsonResponse
+    public function register(Request $request): JsonResponse
     {
-        $request->validate([
-            'name' => 'required|string|max:50',
-            'username' => 'required|string|max:50',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-        ]);
-
-        $user = User::create([
-            'name' => $request->name,
-            'username' => $request->username,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        return response()->json(['user' => $user], 201);
+        return $this->authService->register($request);
     }
 
-    /**
-     * @OA\Post(
-     *     path="/api/login",
-     *     tags={"Auth"},
-     *     summary="Log in a user",
-     *     @OA\RequestBody(
-     *         required=true,
-     *         @OA\JsonContent(
-     *             required={"identity","password"},
-     *             @OA\Property(property="identity", type="string",  example="john@example.com"),
-     *             @OA\Property(property="password", type="string", format="password", example="password"),
-     *             @OA\Property(property="remember", type="boolean",  example="true")
-     *         )
-     *     ),
-     *     @OA\Response(response=200, description="Logged in successfully"),
-     *     @OA\Response(response=401, description="Invalid credentials")
-     * )
-     */
-    public function login(LoginRequest $request): \Illuminate\Http\JsonResponse
+    public function login(LoginRequest $request): JsonResponse
     {
         return $this->authService->login($request);
     }
 
-    /**
-     * @OA\Post(
-     *     path="/api/logout",
-     *     tags={"Auth"},
-     *     summary="Log out a user",
-     *     security={{"sanctum":{}}},
-     *     @OA\Response(response=200, description="Successfully logged out")
-     * )
-     */
-    public function logout(Request $request): \Illuminate\Http\JsonResponse
+    public function logout(Request $request): JsonResponse
     {
         return $this->authService->logout($request);
     }
 
-    public function profile(Request $request):JsonResponse
+    public function profile(Request $request): JsonResponse
     {
         return $this->authService->profile($request);
     }
--- app/Http/Api/Auth/AuthService.php
+++ app/Http/Api/Auth/AuthService.php
@@ -10,39 +10,41 @@ use App\Models\User;
 use App\Traits\ApiResponseFormatTrait;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Validator;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use LaravelLang\Publisher\Concerns\Has;
+use Nette\Schema\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthService
 {
     use ApiResponseFormatTrait;
 
-    public function login(LoginRequest $request):JsonResponse
+    public function login(LoginRequest $request): JsonResponse
     {
         $validated = $request->validated();
         $firstCredentialValue = $validated['identity'];
         $firstCredentialValueType = filter_var($firstCredentialValue, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
         $user = User::where("$firstCredentialValueType", $validated['identity'])->first();
 
-        if(!$user || !Hash::check($validated['password'], $user->password)){
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
             return $this->sendError(
                 ApiResponseMessages::INVALID_CREDENTIALS,
                 ApiResponseCodes::HTTP_UNAUTHORIZED
             );
         }
 
-        $permissions = $user->hasRole('super-admin')? ['*'] : collect($user->getAllPermissions())->pluck('name')->toArray();
+        $permissions = $user->hasRole('super-admin') ? ['*'] : collect($user->getAllPermissions())->pluck('name')->toArray();
 
-        $token = $user->createToken(config('app.name','Backend'),
+        $token = $user->createToken(config('app.name', 'Backend'),
             [...$permissions],
         )->plainTextToken;
         $data = array(
-            'user'=>$user,
-            'token'=>$token,
-            'permissions'=>$permissions
+            'user' => $user,
+            'token' => $token,
+            'permissions' => $permissions
         );
         return $this->sendResponse(
             $data,
@@ -54,24 +56,46 @@ class AuthService
 
     }
 
-    public function register(Request $request)
+    public function register(Request $request, $model = null)
     {
+        try {
+            $user_validator = $request->validate([
+                'username' => 'required|string|min:3|unique:users,username',
+                'name' => 'required|string',
+                'surname' => 'required|string',
+                'email' => 'required|string|email|unique:users,email',
+                'password' => 'required|string|min:5',
+                'avatar' => 'image|mimes:jpeg,png,jpg,gif|max:10240',
+            ]);
+            $user = User::create($user_validator);
+            $permissions = $user->hasRole('super-admin') ? ['*'] : collect($user->getAllPermissions())->pluck('name')->toArray();
 
-//        User::create([
-//            'uuid'=> Str::uuid(),
-//            'username' => "admin",
-//            'name' => 'Subasta',
-//            'surname' => 'Administrator',
-//            'email' => 'admin@subasta.com',
-//            'email_verified_at' => \Carbon\Carbon::now(),
-//            'password' => "password",
-//            'avatar'=>'avatars/default.png'
-//        ]);
+            $token = $user->createToken(config('app.name', 'Backend'),
+                [...$permissions],
+            )->plainTextToken;
+            $data = array(
+                'user' => $user,
+                'token' => $token,
+                'permissions' => $permissions
+            );
+            return $this->sendResponse(
+                $data,
+                ApiResponseMessages::LOGIN_SUCCESSFUL,
+                ApiResponseCodes::HTTP_SUCCESS
+                ,
+                $data
+            );
 
-
+        } catch (ValidationException $ex) {
+            return $this->sendError(
+                ApiResponseMessages::UNPROCESSABLE_CONTENT,
+                ApiResponseCodes::HTTP_UNPROCESSABLE_CONTENT,
+                $ex->errors()
+            );
+        }
     }
 
-    public function logout(Request $request):JsonResponse
+    public function logout(Request $request): JsonResponse
     {
         $request->user()->tokens()->delete();
         return $this->sendResponse(null, ApiResponseMessages::LOGGED_OUT_SUCCESSFULLY);
@@ -83,9 +107,9 @@ class AuthService
 
     }
 
-    public function profile(Request $request):JsonResponse
+    public function profile(Request $request): JsonResponse
     {
-        return $this->sendResponse($request->user(),ApiResponseMessages::FETCHED_SUCCESSFULLY);
+        return $this->sendResponse($request->user(), ApiResponseMessages::FETCHED_SUCCESSFULLY);
     }
 
 
