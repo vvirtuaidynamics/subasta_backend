377acd3cd83ed6d69ba9f1f438ea700e557d57d2
d4ism3l <d4ism3l@gmail.com>
2024-07-03 02:08:52 -0400

    Adicionando funciones get_modules, roles_sync, permissions_sync, get_user_modules.

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -9,6 +9,9 @@
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use App\Models\User;
 
 if (!function_exists('trans_lang_using_default_file')) {
     function trans_lang_using_default_file($key): \Illuminate\Foundation\Application|array|string|\Illuminate\Contracts\Translation\Translator|\Illuminate\Contracts\Foundation\Application|null
@@ -49,7 +52,6 @@ if (!function_exists('get_models')) {
     {
         $models = array_map('basename', glob(app_path() . '/Models/*.php'));
 
-
         //Add spatie models
         $spatie_permission = ['Permission', 'Role'];
         $spatie_activity = ['Activity'];
@@ -73,14 +75,11 @@ if (!function_exists('get_models')) {
             $table = (new $modelClass)->getTable();
             $columns = Schema::getColumnListing($table);
 
-
             if ($modelClass instanceof \Illuminate\Database\Eloquent\Model) {
                 $model_fields = [];
                 $class_name = class_basename($modelClass);
                 if (in_array($model, $spatie_permission))
                     $class_namespace = "Spatie\\Permission\\Models\\$model";
-                elseif (in_array($model, $spatie_activity))
-                    $class_namespace = "Spatie\\Activitylog\\Models\\$model";
                 else
                     $class_namespace = "App\\Models\\$model";
 
@@ -89,65 +88,141 @@ if (!function_exists('get_models')) {
 
                     $model_fields[] = ["column" => $column, "type" => $type];
                 }
-                $schemas[] = ["name" => $model, "namespace" => $class_namespace, "fields" => $model_fields];
+                $schemas[] = ["name" => $model, "class" => $class_namespace, "fields" => $model_fields];
                 if ($model_name && $model_name == $model) {
                     return ["column" => $column, "type" => $type];
                 }
             }
-
-
         }
         return $schemas;
     }
 }
-
-if (!function_exists('get_user_models')) {
-    function get_user_models(\App\Models\User $user = null): array
+if (!function_exists('get_modules')) {
+    function get_modules(): array
     {
-//        if (!$user) return [];
-
-        $appList = collect([]);
-        $applications = collect(DB::table('modules')->where('parent_name', '=', '')->orderBy('order', 'asc')->get());
-
-        $application_names = collect($applications)->pluck('name')->toArray();
-        foreach ($applications as $application) {
-            $modules = DB::table('modules')->where('parent_name', '=', $application->name)->orderBy('order', 'asc')->get();
-            dd($modules);
-            $module_names = collect($modules)->pluck('name')->toArray();
-            $app = $application;
-
-            $app['children'] = [];
-            foreach ($modules as $module) {
-                if (!$user->is_super_admin) {
-                    $module['permissions'] = ['create' => true, 'read' => true, 'update' => true, 'delete' => true];
-                    $app['children'] = $module;
+        $modulos = config('modules.data');
+        $models = get_models();
+        $modules = [];
+        if (isset($modulos) && is_array($modulos))
+            foreach ($modulos as $modulo) {
+                $modulo_model = null;
+                $m = [
+                    'name' => $modulo['name'],
+                    'model' => $modulo['model'],
+                    'class' => $modulo['class'],
+                    'label' => $modulo['label'],
+                    'title' => $modulo['title'],
+                    'url' => $modulo['url'],
+                    'icon' => $modulo['icon'],
+                    'order' => $modulo['order'],
+                    'parent' => $modulo['parent'],
+                    'fields' => $modulo['fields'],
+                    'permissions' => json_encode($modulo['permissions']),
+                ];
+                if ($m['model'] != '') {
+                    $modulo_model = collect(array_filter($models, function ($model) use ($modulo) {
+                        return strtolower($model['name']) === strtolower($modulo['name']);
+                    }))->first();
+                    if (isset($modulo_model)) {
+                        if (isset($modulo_model['fields']) && $modulo_model['fields'] != '')
+                            $m['fields'] = json_encode($modulo_model['fields']);
+                        else {
+                            $m['fields'] = "{}";
+                        }
+                        // Validate class
+                        if (!isset($m['class']) || !class_exists($m['class'])) {
+                            if (class_exists($modulo_model->class)) {
+                                $m['class'] = $modulo_model->class;
+                            }
+                        }
+                    }
                 }
-                //todo continuar con la logica
-
-
+                $modules[] = $m;
             }
+        return $modules;
+    }
+}
 
-
+if (!function_exists('permissions_sync')) {
+    function permissions_sync(): array
+    {
+        $modules = get_modules();
+        $super_admin = \Spatie\Permission\Models\Role::findOrCreate(config('permission.super_admin_role_name'), 'api');
+        $start_permissions = Permission::all();
+        foreach ($modules as $module) {
+            $name = $module['name'];
+            $permissions = json_decode($module['permissions']);
+            if (isset($permissions) && is_array($permissions))
+                foreach ($permissions as $p) {
+                    $permission = Permission::findOrCreate("$name:$p", 'api');
+                    $super_admin->givePermissionTo($permission);
+                }
         }
+        $final_permissions = Permission::all();
+        $diff_permissions = $final_permissions->diff($start_permissions);
+        return [
+            'permissions' => $final_permissions,
+            'changes' => $diff_permissions,
+            'total' => $final_permissions->count(),
+            'diff' => $final_permissions->count() - $start_permissions->count(),
+        ];
+    }
+}
 
-        $actions_default = ['create', 'read', 'update', 'delete'];
-        $actions_readonly = ['read'];
+if (!function_exists('roles_sync')) {
+    function roles_sync(): array
+    {
+        $super_admin = Role::findOrCreate(config('permission.super_admin_role_name'), 'api');
+        $start_roles = Role::all();
+        $config_roles = config('permission.base_roles');
+        foreach ($config_roles as $role) {
+            $role_name = $role['name'];
+            $rol = Role::findOrCreate($role_name, 'api');
+            $permissions = $role['permissions'];
+            if (isset($permissions) && is_array($permissions))
+                //TODO: Se puede implementar lógica para usar comodines para modelos o permisos
+                // Ejemplo:
+                foreach ($permissions as $p) {
+                    $permission = Permission::findOrCreate("$p", 'api');
+                }
+        }
+        $final_roles = Role::all();
+        $diff_roles = $final_roles->diff($start_roles);
+        return [
+            'roles' => $final_roles,
+            'changes' => $diff_roles,
+            'total' => $final_roles->count(),
+            'diff' => $final_roles->count() - $start_roles->count(),
+        ];
+    }
+}
 
+if (!function_exists('get_user_modules')) {
+    function get_user_modules(User $user = null): array
+    {
+        if (!$user) return [];
         $user_permissions = collect($user->getAllPermissions())->pluck('name')->toArray();
-
-        $application = DB::table('modules')->where('parent_name', '!=', '')->orderBy('order', 'asc')->get();
-        dd($application);
-//        if ($application->count > 0) {
-//            $app_models = DB::table('modules')->where('parent',)
-//        }
-//
-//        if(){
-//
-//        }
-
-        $schemas = [];
-
-
+        $user_modules = [];
+        $modules = get_modules();
+        foreach ($modules as $module) {
+            $name = $module['name'];
+            $module_permissions = [];
+            if (isset($module['permissions']) && $module['permissions'] != '' && $module['permissions']) {
+                $module_permissions = json_decode($module['permissions']);
+            }
+            $permisions = [];
+            $module_has_per = 0;
+            foreach ($module_permissions as $p) {
+                $pname = "$name:$p";
+                $pval = ($user->super_admin || in_array("$name:$p", $user_permissions));
+                if ($pval) $module_has_per++;
+                $permisions[] = [$pname => $pval];
+            }
+            $module['permissions'] = $permisions;
+            if ($module_has_per > 0)
+                $user_modules[] = $module;
+        }
+        return $user_modules;
     }
 }
 
