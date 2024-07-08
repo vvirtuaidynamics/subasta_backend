d0f8663edb4bbb76ce96f0a6b261fd28b21e8abd
d4ism3l <d4ism3l@gmail.com>
2024-07-08 16:21:52 -0400

    Migración de bearing

--- database/migrations/2024_06_17_210645_create_bearings_table.php
+++ database/migrations/2024_06_17_210645_create_bearings_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,20 @@ return new class extends Migration
     {
         Schema::create('bearings', function (Blueprint $table) {
             $table->id();
+            $table->string('weight');
+            $table->string('wide');
+            $table->string('long');
+            $table->string('height');
+            $table->string('total_packages');
+            $table->text('content_description');
+            $table->string('declared_value');
+            $table->string('service_type');
+            $table->boolean('shipping_insurance');
+            $table->boolean('weekend_delivery');
+            $table->boolean('home_pickup');
+            $table->string('observations');
+            $table->string('status');
+
             $table->timestamps();
         });
     }
