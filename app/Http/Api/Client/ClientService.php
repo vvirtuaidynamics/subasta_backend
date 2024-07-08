5c0752cece3523d589e0e2bf548577f48353c224
d4ism3l <d4ism3l@gmail.com>
2024-07-08 16:20:14 -0400

    Creación de la regla para Client

--- app/Http/Api/Client/ClientService.php
+++ app/Http/Api/Client/ClientService.php
@@ -23,9 +23,23 @@ class ClientService extends BaseService
     public function rules(): array
     {
         return [
-            "name" => "required",
-            "email" => "required",
-            "password" => "required"
+            'username' => 'required|string|max:50|unique:users',
+            'name' => 'required|string|max:50',
+            'surname' => 'nullable|string|max:50',
+            'email' => 'required|string|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed', // 'have to receive password_confirmation'
+            'active' => 'boolean|nullable',
+            'avatar' => 'image|nullable|mimes:jpeg,png,jpg,gif|max:2048',
+
+            "address" => "required|string",
+            "phone" => "required|string",
+            "date_of_birth" => "required|date",
+            "company_name" => "nullable|string",
+            "industry" => "nullable|string",
+            "user_id" => "nullable|numeric",
+            "about_me" => "nullable|string",
+            "photo" => "nullable|string",
+            "gender" => "required|in:'unknown,male,female'",
         ];
     }
 
