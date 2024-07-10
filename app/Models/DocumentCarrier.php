6389dc31090546ec93c7a9a10cdbb0e0d8a948f8
d4ism3l <d4ism3l@gmail.com>
2024-07-10 00:22:08 -0400

    Add DocumentCarrier.php

--- app/Models/DocumentCarrier.php
+++ app/Models/DocumentCarrier.php
@@ -8,21 +8,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Http\Api\Base\BaseModel;
+use Illuminate\Support\Facades\Storage;
 
 
 class DocumentCarrier extends BaseModel
 {
     use HasFactory, SoftDeletes;
 
-    protected $guarded = [];
-
+    protected $fillable = ['name', 'type', 'path', 'size', 'preview'];
+    protected $appends = ['file_url'];
 
     public function carrier(): BelongsTo
     {
         return $this->belongsTo(Carrier::class, 'carrier_id');
     }
 
-    //Casts
     protected function expireDate(): Attribute
     {
         return Attribute::make(
@@ -35,5 +35,10 @@ class DocumentCarrier extends BaseModel
         return Attribute::make(
             get: fn($value) => $value ? Carbon::parse($value)->locale(app()->getLocale())->format(datetime_format_string()) : null
         );
+    }
+
+    public function getFileUrlAttribute()
+    {
+        return Storage::url($this->path);
     }
 }
