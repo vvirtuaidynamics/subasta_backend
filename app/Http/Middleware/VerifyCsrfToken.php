458eff8102177b3037680b58028ce765206450eb
d4ism3l <d4ism3l@gmail.com>
2024-08-06 16:38:37 -0400

    fix!: Disable csrf check for debug with postman

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,5 +13,8 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
+        // TODO: QUITAR EN PRODUCCION
+        'api/*', // Permitir todas las rutas de la API
+        'doc/*', // Permitir todas las rutas de la Documentacion
     ];
 }
