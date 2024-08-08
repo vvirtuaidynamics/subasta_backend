95d3ff7ed1ccdcc833cc3123af40e8349907ad9f
d4ism3l <d4ism3l@gmail.com>
2024-08-08 08:00:04 -0400

    fix!: Fix and changes for get countries translations and image on select. Check add mode to toggle between check unique and exist.

--- app/Http/Api/Base/HelperController.php
+++ app/Http/Api/Base/HelperController.php
@@ -13,8 +13,9 @@ use App\Http\Controllers\Controller;
 
 class HelperController extends Controller
 {
-    public function check_unique(Request $request): JsonResponse
+    public function check(Request $request): JsonResponse
     {
+        $mode = $request->input('mode') ?? 'unique';
         $exist = false;
         if ($request->has('table') && $request->has('column') && $request->has('value')) {
             $table = $request->input('table');
@@ -22,6 +23,7 @@ class HelperController extends Controller
             $value = $request->input('value');
             $exist = unique_check($table, $column, $value);
         }
+        if ($request->has('mode') && $request->input('mode') === 'exist') return response()->json(['exist' => $exist]);
         return response()->json(['unique' => !$exist]);
     }
 
@@ -31,24 +33,33 @@ class HelperController extends Controller
         $table = $request->input('table');
         $column_id = $request->input('column_id') ?? 'id';
         $column_label = $request->input('column_label') ?? 'name';
-
         $column_image = $request->input('column_image') ?? null;
+        $locale = $request->input('locale') ?? null;
         $filter = $request->input('filter') ?? '';
         $data = [];
         if (Schema::hasColumn("$table", "$column_id") && Schema::hasColumn("$table", "$column_label")) {
-            DB::table($table)->orderBy($column_label)->where($column_label, 'like', $filter . '%')->chunk($chuckSize, function (Collection $items) use ($column_id, $column_label, $data, $column_image) {
-
-                foreach ($items as $item) {
-                    $predata = [
-                        'value' => $item->$column_id,
-                        'label' => $item->$column_label
-                    ];
-                    if ($column_image)
-                        $predata['image'] = $item->$column_image;
-                    $data[] = $predata;
-                }
-                return response()->json($data);
-            });
+            DB::table($table)->orderBy($column_label)->where($column_label, 'like', $filter . '%')
+                ->chunk($chuckSize, function (Collection $items) use ($column_id, $column_label, &$data, $locale, $table, $column_image) {
+                    foreach ($items as $item) {
+                        $translations = null;
+                        $predata = [
+                            'value' => $item->$column_id,
+                            'label' => $item->$column_label
+                        ];
+                        if ($table == 'countries') {
+                            $translations = json_decode($item->translations);
+                            $predata['translations'] = $translations;
+                            if ($locale && isset($translations->$locale)) {
+                                $predata['label'] = $translations->$locale;
+                            }
+                            $predata['image'] = asset('images/flags/' . strtolower($item->iso2) . '.svg');
+                        }
+                        if ($column_image)
+                            $predata['image'] = $item->$column_image;
+                        $data[] = $predata;
+                    }
+                });
+            return response()->json($data);
         } else {
             return response()->json([]);
 
