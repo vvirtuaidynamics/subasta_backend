47660ce05107574299077e018db3ced2165fe09d
d4ism3l <d4ism3l@gmail.com>
2024-08-16 08:37:51 -0400

    fix!: Modelo field

---
+++ app/Models/Field.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Field extends Model
+{
+    use HasFactory;
+
+    protected $table = 'fields';
+    protected $fillable = ['name', 'label', 'placeholder', 'component', 'type', 'include', 'options', 'default_value'];
+
+
+    public function forms(): BelongsToMany
+    {
+        return $this->belongsToMany(Form::class)->withPivot(['options', 'rules', 'step', 'panel', 'position'])->withTimestamps();
+    }
+
+    public function setSlugAttribute($value)
+    {
+        $this->attributes['slug'] = slugify($value);
+    }
+
+    public function setOptionsAttribute($value)
+    {
+        if (!isset($value) || $value === null || $value === '') $this->attributes['options'] = "{}";
+        $this->attributes['options'] = json_encode($value);
+    }
+
+    public function getOptionsAttribute($value)
+    {
+        if (!isset($value) || $value === null || $value === '') $value = "{}";
+        return json_decode($value);
+    }
+}
